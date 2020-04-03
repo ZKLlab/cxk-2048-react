f85c497a7e546b6763cd9f1c238e9fe9389985fa
钟坤林 <zkl@zkllab.com>
2020-04-03 13:35:27 +0800

    Updates

---
+++ precache-manifest.a196ccbaa32de56c2804a487519679a4.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c279ad2da0ccfd5dd109dd939b591008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef0d88e81d100e79d11e",
+    "url": "/static/css/main.d541e16d.chunk.css"
+  },
+  {
+    "revision": "27cae61823df6fe0d0d1",
+    "url": "/static/js/2.38403245.chunk.js"
+  },
+  {
+    "revision": "e118b7ec0bf4ef523a6699e6fb0acd0e",
+    "url": "/static/js/2.38403245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0d88e81d100e79d11e",
+    "url": "/static/js/main.a0b0877c.chunk.js"
+  },
+  {
+    "revision": "090b8fd8ef305cfdc0a4",
+    "url": "/static/js/runtime-main.d48d4c86.js"
+  },
+  {
+    "revision": "90ca6798a680ea5642b6a36711c10438",
+    "url": "/static/media/NotoSansHans-Medium.90ca6798.woff"
+  },
+  {
+    "revision": "94991035a295267ee308cf04e559fbe4",
+    "url": "/static/media/NotoSansHans-Medium.94991035.ttf"
+  },
+  {
+    "revision": "b548ae10a6dbb1d78539adc61dd991a6",
+    "url": "/static/media/NotoSansHans-Medium.b548ae10.svg"
+  },
+  {
+    "revision": "fb95f6035e868396c401c3e9243dbfc4",
+    "url": "/static/media/NotoSansHans-Medium.fb95f603.eot"
+  },
+  {
+    "revision": "149bec9e3d1df3101747d13ccc7ea3cb",
+    "url": "/static/media/OpenSans-Semibold.149bec9e.svg"
+  },
+  {
+    "revision": "31a82a9bde90eec038693f26ada892ad",
+    "url": "/static/media/OpenSans-Semibold.31a82a9b.eot"
+  },
+  {
+    "revision": "51b15d272cef490c68a298039ae41676",
+    "url": "/static/media/OpenSans-Semibold.51b15d27.ttf"
+  },
+  {
+    "revision": "53823acbd12d962abd2893905cba9df5",
+    "url": "/static/media/OpenSans-Semibold.53823acb.woff"
+  },
+  {
+    "revision": "59d1fe1ec9438eaed2fbc89a11204b2f",
+    "url": "/static/media/OpenSans-Semibold.59d1fe1e.woff2"
+  },
+  {
+    "revision": "a0467517868f8cbb93f769d0fe726abd",
+    "url": "/static/media/effect-1024.a0467517.mp3"
+  },
+  {
+    "revision": "7d9cd61e3a52dde238b296ec1da04d92",
+    "url": "/static/media/effect-128.7d9cd61e.mp3"
+  },
+  {
+    "revision": "0953911a1df69e69a4376c9f7b01eecd",
+    "url": "/static/media/effect-16.0953911a.mp3"
+  },
+  {
+    "revision": "4853522c292063bd3dd40c3ffd276901",
+    "url": "/static/media/effect-2.4853522c.mp3"
+  },
+  {
+    "revision": "cbd9f9d4e129e31fbaa80209be251195",
+    "url": "/static/media/effect-2048.cbd9f9d4.mp3"
+  },
+  {
+    "revision": "7e6f0524c3a30a6601998ffabcb44cd0",
+    "url": "/static/media/effect-256.7e6f0524.mp3"
+  },
+  {
+    "revision": "a702a9c869a2470c1c806c5f5237e081",
+    "url": "/static/media/effect-32.a702a9c8.mp3"
+  },
+  {
+    "revision": "2c69c693632cef6a1163bfc5c85b40f0",
+    "url": "/static/media/effect-4.2c69c693.mp3"
+  },
+  {
+    "revision": "e0a903fa36b7b4931e7e23a4d419ef0d",
+    "url": "/static/media/effect-4096.e0a903fa.mp3"
+  },
+  {
+    "revision": "15364d6c543708273b74920b6573a700",
+    "url": "/static/media/effect-512.15364d6c.mp3"
+  },
+  {
+    "revision": "ef7ffd6add74a7091377c40e87e08e91",
+    "url": "/static/media/effect-64.ef7ffd6a.mp3"
+  },
+  {
+    "revision": "757f5d765ceba42e282d258fa6fb429e",
+    "url": "/static/media/effect-8.757f5d76.mp3"
+  },
+  {
+    "revision": "4b3c7e4651e2027e2a18a52521d200b9",
+    "url": "/static/media/game-over.4b3c7e46.png"
+  },
+  {
+    "revision": "b02ed4023568d4fa6022064f194fb4f5",
+    "url": "/static/media/win.b02ed402.png"
+  }
+]);
